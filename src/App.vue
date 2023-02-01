05f0e743b7a5e75659a3a7253b17c84a5cb328d4
Diletta Pelloni <dilpel@live.it>
2023-02-01 15:02:37 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import HelloWorld from './components/HelloWorld.vue';
+  export default {
+      components: {
+        HelloWorld
+      },
+      data() {
+        return {
+            
+        }
+      }
+  }
+</script>
+
+<template>
+
+  <HelloWorld/>
+
+</template>
+
+<style scoped>
+
+</style>
